f5cc8e96b6d4af6d97c97a0d6a36fc416692dfac
Tinker-Bits <jashupadhyay.java@gmail.com>
2025-05-16 10:29:39 +0530

    Material App And Scaffold Widget Added

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,25 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp());
+  runApp(MatApp());
+}
+
+class MatApp extends StatelessWidget {
+  const MatApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaff(),
+      title: 'Dice Roll',
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class Scaff extends StatelessWidget {
+  const Scaff({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
 }
